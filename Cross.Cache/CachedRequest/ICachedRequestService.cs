72ab524031397352fa8b26fc14e44217af62d733
Denis Peshkov <denis.peshkov@outlook.com>
2024-06-22 17:41:44 +0300

    Add CachedRequestService (#48)

---
+++ Cross.Cache/CachedRequest/ICachedRequestService.cs
@@ -0,0 +1,7 @@
+﻿namespace Cross.Cache.CachedRequest;
+
+public interface ICachedRequestService<out TCachedValue>
+    where TCachedValue : class
+{
+    TCachedValue? GetAll(bool force = false);
+}
